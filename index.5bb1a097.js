bfb06b125a54f0c76549dc70db17f6e1f0b323f0
VikiBay <102660491+VikiBay@users.noreply.github.com>
2022-11-29 19:51:11 +0000

    Deploying to gh-pages from @ VikiBay/goit-js-hw-11@adf60581be1db3e0df45b0270ab78a084f648d2e 🚀

--- index.5bb1a097.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault(),console.log("submit")}));
-//# sourceMappingURL=index.5bb1a097.js.map
--- index.f2f41908.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-form").addEventListener("submit",(function(e){e.preventDefault(),console.log("submit")}));
-//# sourceMappingURL=index.f2f41908.js.map
